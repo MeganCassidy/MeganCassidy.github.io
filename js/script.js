a42d366b5becd8289e4b9da7cd4f1ea5734e294d
Megan Cassidy <61455706+MeganCassidy@users.noreply.github.com>
2020-10-09 17:06:13 +0100

    project info added

--- js/script.js
+++ js/script.js
@@ -27,3 +27,54 @@ function complete(){
     clearInterval(timer);
     timer = null;
 }
+
+//info button Coffee House
+
+$(document).ready(function(){
+    $(".info1").click(function(){
+      $(".infoDiv1").show();
+      $("#coffee").hide();
+      $("#coffeeMob").hide();
+    });
+    $(".close1").click(function(){
+      $(".infoDiv1").hide();
+      $("#coffee").show();
+      $("#coffeeMob").show();
+    });
+  });
+  
+
+  //info button Travel Bug
+
+$(document).ready(function(){
+    $(".info2").click(function(){
+      $(".infoDiv2").show();
+      $("#travel").hide();
+      $("#travelMob").hide();
+    });
+    $(".close2").click(function(){
+      $(".infoDiv2").hide();
+      $("#travel").show();
+      $("#travelMob").show();
+    });
+  });
+
+  //info button PW Services 
+
+$(document).ready(function(){
+    $(".info3").click(function(){
+      $(".infoDiv3").show();
+      $("#pwServices").hide();
+      $("#pwServicesMob").hide();
+    });
+    $(".close3").click(function(){
+      $(".infoDiv3").hide();
+      $("#pwServices").show();
+      $("#pwServicesMob").show();
+    });
+  });
+
+  
+  
+  
+
